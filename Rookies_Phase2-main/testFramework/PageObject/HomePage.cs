bec34d64139c76c10d4baecc671dc9381427c4fb
tienanh102001 <dinhtienanh10tmu@gmail.com>
2022-11-30 08:09:04 +0700

    test project

--- Rookies_Phase2-main/testFramework/PageObject/HomePage.cs
+++ Rookies_Phase2-main/testFramework/PageObject/HomePage.cs
@@ -17,10 +17,10 @@ namespace testFramework.PageObject
         private readonly string btnMenu = "//li[@role='none']";
 
         private readonly string btnChangePass = "//div[contains(@style,'position')]//li[1]";
-        private readonly string boxOldPass = "//input[@id='change-password_oldPassword']";
+        private readonly string boxCurrentPass = "//input[@id='change-password_oldPassword']";
         private readonly string boxNewPass = "//input[@id='change-password_newPassword']";
-        private readonly string btnSaveChange = "//button[@class='ant-btn ant-btn-primary ant-btn-dangerous']";
-        private readonly string btnCancelChange = "//button[@class='ant-btn ant-btn-default']";
+        private readonly string btnSave = "//button[@class='ant-btn ant-btn-primary ant-btn-dangerous']";
+        private readonly string btnCancel = "//button[@class='ant-btn ant-btn-default']";
 
         private readonly string btnLogout = "//div[contains(@style, 'position')]//li[2]";
         private readonly string btnConfirmLog = "//span[text()='Log out']";
